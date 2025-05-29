90ade85344b488738781d42bd0b4c38c01b9a195
Jonnits <jonathanrubra@gmail.com>
2025-05-28 17:19:38 -0700

    Created getAccessToken function

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -5,9 +5,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = [
- "https://meet-beryl-seven.vercel.app/"
-];
+const redirect_uris = ["https://meet-beryl-seven.vercel.app"];
 
 
 const oAuth2Client = new google.auth.OAuth2(
@@ -35,4 +33,40 @@ module.exports.getAuthURL = async () => {
      authUrl,
    }),
  };
+};
+
+
+
+module.exports.getAccessToken = async (event) => {
+// Decode authorization code extracted from the URL query
+const code = decodeURIComponent(`${event.pathParameters.code}`);
+    
+    
+return new Promise((resolve, reject) => {
+    
+oAuth2Client.getToken(code, (error, response) => {
+    if (error) {
+    return reject(error);
+    }
+    return resolve(response);
+});
+})
+.then((results) => {
+    // Respond with OAuth token
+    return {
+    statusCode: 200,
+    headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+    },
+    body: JSON.stringify(results),
+    };
+})
+.catch((error) => {
+    // Handle error
+    return {
+    statusCode: 500,
+    body: JSON.stringify(error),
+    };
+  });
 };
