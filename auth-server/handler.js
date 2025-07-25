17c94eb4b31c95060386ff9c78cc28aba0797125
Jonnits <jonathanrubra@gmail.com>
2025-07-25 16:47:59 -0700

    Updated handler.js to deploy from github

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -4,7 +4,7 @@ const { google } = require("googleapis");
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.events.public.readonly"];
 const { CLIENT_SECRET, CLIENT_ID, CALENDAR_ID } = process.env;
-const redirect_uris = ["https://meet-beryl-seven.vercel.app"];
+const redirect_uris = ["https://jonnits.github.io/meet/"];
 
 const oAuth2Client = new google.auth.OAuth2(
  CLIENT_ID,
